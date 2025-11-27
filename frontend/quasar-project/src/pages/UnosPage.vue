0986f35a8d0188ba40f30953f91144b7d0d43b35
tomass17 <tomas.grgic17@gmail.com>
2025-11-27 13:24:22 +0100

    ishod2test

---
+++ frontend/quasar-project/src/pages/UnosPage.vue
@@ -0,0 +1,8 @@
+<template>
+  <q-page class="q-pa-md">
+    <q-btn label="Klikni me" color="primary" ></q-btn>
+  </q-page>
+</template>
+
+<script setup>
+</script>
---
+++ frontend/quasar-project/src/router/routes.js
@@ -0,0 +1,17 @@
+const routes = [
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      { path: 'unospage', component: () => import('pages/UnosPage.vue') },
+    ],
+  },
+
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/ErrorNotFound.vue'),
+  },
+]
+
+export default routes
